8d748fce237ad59c2522f029d901f112845bd90b
cheojorge <cheojorge@hotmail.com>
2022-01-24 22:22:31 -0500

    End

---
+++ pai-de-pokemon/src/App.js
@@ -0,0 +1,13 @@
+
+import { Poke } from './ Components/Poke';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Poke/>
+    </div>
+  );
+}
+
+export default App;
