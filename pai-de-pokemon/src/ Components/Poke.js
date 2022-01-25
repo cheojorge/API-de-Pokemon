51993d2466128e3e18c5ece2e625efd89d646ec7
cheojorge <cheojorge@hotmail.com>
2022-01-24 22:21:44 -0500

    Finish

---
+++ pai-de-pokemon/src/ Components/Poke.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+export const Poke = () => {
+    const [pokemon, setPokemon] = useState([])
+    const [click, setclick] = useState(false)
+    useEffect(() => {
+        console.log('entro')
+        if(click){
+            fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=807')
+            .then(response => response.json())
+            .then(response => setPokemon(response.results))
+        }
+        
+    }, [click])
+    console.log(click)
+    return (
+        <div>
+            <button onClick={() => setclick(true)}>Fetch Pokemon</button>
+            <ol>
+                {
+                    pokemon.map((pokemon,index)=>{
+                        return(<li key={index}>{pokemon.name}</li>)
+                    })
+                }
+            </ol>
+        </div>
+    )
+}
